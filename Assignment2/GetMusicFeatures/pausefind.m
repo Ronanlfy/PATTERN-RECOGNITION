74e9197e8725d019900a795aceeef8bc4d9cf824
Feiyang <liuf@kth.se>
2017-10-04 22:24:52 +0200

    Add files via upload

--- Assignment2/GetMusicFeatures/pausefind.m
+++ Assignment2/GetMusicFeatures/pausefind.m
@@ -1,5 +1,5 @@
  
-function output = pausefind(frIsequence,sigma,theta)
+function [freq,sigma,theta] = pausefind(frIsequence)
 %output = pausefind(frIsequence,sigma,theta) used for find the
 %pause/silent region from the original melody input. 
 %
@@ -15,29 +15,37 @@ function output = pausefind(frIsequence,sigma,theta)
 %Code Authors:FEIYANG LIU
 %----------------------------------------------------
 
-length = size(frIsequence,2);
-
 freq = frIsequence(1,:);
 corr = frIsequence(2,:);
 int = frIsequence(3,:);
 
 lowbound = min(freq) / 2;
 
+%normalize the intensity into interval[0,1]
 a = max(int);
 b = min(int);
 newint = (int - b) / (a - b);
 
-%find the low intensity area
+%these two bounds mean that when value is larger than that, we 100% believe
+%it is melody instead of noises
+bound_int = 0.8;
+bound_corr = 0.95;
+
+%find the low intensity area, and also the threshold for its intensity.
+%here we delete all the area where we believe they are melody, and get the
+%mean of left regions as the threshold.
+unuse_newint = find(newint < bound_int);
+theta = mean(newint(unuse_newint));
 pause = find(newint < theta);
 freq(pause) = lowbound;
-%freq(pause) = lowbound + (upperbound - lowbound) * rand(1,size(pause,2));
 
 %find the low correlation area
+unuse_corr = find(corr < bound_corr);
+sigma = mean(corr(unuse_corr));
 pausefind = find(corr < sigma);
-freq(pause) = lowbound;
-%freq(pause) = lowbound + (upperbound - lowbound) * rand(1,size(pause,2));
+freq(pausefind) = lowbound;
 
-output = freq;
+
 end
 
 
