10aadeb10046f183257f5b34e028b0ba8d13924b
Feiyang <liuf@kth.se>
2017-10-04 22:25:21 +0200

    Add files via upload

--- Assignment2/GetMusicFeatures/main.m
+++ Assignment2/GetMusicFeatures/main.m
@@ -20,9 +20,10 @@ filename3 = 'melody_3.wav';
 seq3 = GetMusicFeatures(y3,fs3);
 T3 = [1 : size(seq3,2)] / size(seq3,2) * (length(y3) / fs3);
 
-%set parameters for feature extractor
-sigma = 0.77;
-theta = 0.33;
+
+[feature1,sigma1,theta1] = FeatureExtract(seq1);
+[feature2,sigma2,theta2] = FeatureExtract(seq2);
+[feature3,sigma3,theta3] = FeatureExtract(seq3);
 
 
 %three pitches after y-axis log
@@ -59,7 +60,7 @@ figure(2)
 subplot(3,1,1)
 plot(T1,seq1(2,:),'b');
 hold on 
-plot(T1,repmat(sigma,1,size(seq1(2,:),2)),'--r');
+plot(T1,repmat(sigma1,1,size(seq1(2,:),2)),'--r');
 grid on 
 title('correlation of melody1');
 legend('correlation','threshold sigma')
@@ -69,7 +70,7 @@ ylabel('correlation');
 subplot(3,1,2)
 plot(T2,seq2(2,:),'b');
 hold on 
-plot(T2,repmat(sigma,1,size(seq2(2,:),2)),'--r');
+plot(T2,repmat(sigma2,1,size(seq2(2,:),2)),'--r');
 grid on 
 title('correlation of melody2')
 xlabel('timeT/s');
@@ -78,7 +79,7 @@ ylabel('correlation');
 subplot(3,1,3)
 plot(T3,seq3(2,:),'b');
 hold on 
-plot(T3,repmat(sigma,1,size(seq3(2,:),2)),'--r');
+plot(T3,repmat(sigma3,1,size(seq3(2,:),2)),'--r');
 grid on 
 title('correlation of melody3')
 xlabel('timeT/s');
@@ -118,7 +119,7 @@ b1 = min(seq1(3,:));
 newint1 = (seq1(3,:) - b1) / (a1 - b1);
 plot(T1,newint1,'b');
 hold on 
-plot(T1,repmat(theta,1,size(seq1(2,:),2)),'--r');
+plot(T1,repmat(theta1,1,size(seq1(2,:),2)),'--r');
 grid on 
 title('normalized intensity of melody1');
 legend('normalized intensity','threshold theta')
@@ -131,7 +132,7 @@ b2 = min(seq2(3,:));
 newint2 = (seq2(3,:) - b2) / (a2 - b2);
 plot(T2,newint2,'b');
 hold on 
-plot(T2,repmat(theta,1,size(seq2(2,:),2)),'--r');
+plot(T2,repmat(theta2,1,size(seq2(2,:),2)),'--r');
 grid on 
 title('normalized intensity of melody2');
 xlabel('timeT/s');
@@ -143,39 +144,53 @@ b3 = min(seq3(3,:));
 newint3 = (seq3(3,:) - b3) / (a3 - b3);
 plot(T3,newint3,'b');
 hold on 
-plot(T3,repmat(theta,1,size(seq3(2,:),2)),'--r');
+plot(T3,repmat(theta3,1,size(seq3(2,:),2)),'--r');
 grid on 
 title('normalized intensity of melody3');
 xlabel('timeT/s');
 ylabel('intensity');
 
-
-feature1 = FeatureExtract(seq1,sigma,theta);
-feature2 = FeatureExtract(seq2,sigma,theta);
-feature3 = FeatureExtract(seq3,sigma,theta);
+max1 = max(feature1);
+min1 = min(feature1);
+max2 = max(feature2);
+min2 = min(feature2);
+max3 = max(feature3);
+min3 = min(feature3);
 
 figure(5)
 
 subplot(3,1,1)
 plot(T1,feature1,'b');
-set(gca,'YScale','log')
+%set(gca,'YScale','log')
 %ylim([0, 1]);
+hold on 
+plot(T1,repmat(max1,1,size(seq1(2,:),2)),'--r');
+hold on 
+plot(T1,repmat(min1,1,size(seq1(2,:),2)),'--r');
 title('feature semitones of melody1')
 xlabel('timeT/s');
 ylabel('feature based on pitch');
 
 subplot(3,1,2)
 plot(T2,feature2,'b');
-set(gca,'YScale','log')
+%set(gca,'YScale','log')
 %ylim([0, 1]);
+hold on 
+plot(T2,repmat(max2,1,size(seq2(2,:),2)),'--r');
+hold on 
+plot(T2,repmat(min2,1,size(seq2(2,:),2)),'--r');
 title('feature semitones of melody2')
 xlabel('timeT/s');
 ylabel('feature based on pitch');
 
 subplot(3,1,3)
 plot(T3,feature3,'b');
-set(gca,'YScale','log')
+%set(gca,'YScale','log')
 %ylim([0, 1]);
+hold on 
+plot(T3,repmat(max3,1,size(seq3(2,:),2)),'--r');
+hold on 
+plot(T3,repmat(min3,1,size(seq3(2,:),2)),'--r');
 title('feature semitones of melody3')
 xlabel('timeT/s');
 ylabel('feature based on pitch');
@@ -183,22 +198,30 @@ ylabel('feature based on pitch');
 %test it is roboustnesss to transposition
 transSeq = seq1;
 transSeq(1,:) = seq1(1,:) * 1.5;
-trans_feature = FeatureExtract(transSeq,sigma,theta);
+[trans_feature,] = FeatureExtract(transSeq);
 
 figure(6)
 
 subplot(2,1,1)
 plot(T1,feature1,'b');
-set(gca,'YScale','log')
+%set(gca,'YScale','log')
 %ylim([0, 1]);
+hold on 
+plot(T1,repmat(max1,1,size(seq1(2,:),2)),'--r');
+hold on 
+plot(T1,repmat(min1,1,size(seq1(2,:),2)),'--r');
 title('feature semitones of melody1')
 xlabel('timeT/s');
 ylabel('feature based on pitch');
 
 subplot(2,1,2)
 plot(T1,trans_feature,'b');
-set(gca,'YScale','log')
+%set(gca,'YScale','log')
 %ylim([0, 1]);
+hold on 
+plot(T1,repmat(max1,1,size(seq1(2,:),2)),'--r');
+hold on 
+plot(T1,repmat(min1,1,size(seq1(2,:),2)),'--r');
 title('feature semitones of melody1 after transposition')
 xlabel('timeT/s');
 ylabel('feature based on pitch');
